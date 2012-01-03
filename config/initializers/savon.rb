5e2bd7e48242251926df8e3f55b378d9d8d3f2f7
bzhang <bzhang@optaros.com>
2012-01-03 12:39:54 -0500

    first commit

---
+++ config/initializers/savon.rb
@@ -0,0 +1,5 @@
+Savon.configure do |config|
+      config.log = false            # disable logging
+      #config.log_level = :info      # changing the log level
+      #config.logger = Rails.logger  # using the Rails logger
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magento::Application.config.secret_token = 'f5bf8548916d2e91f817acb8f7f79de272175f541477e1d179535ac5a4acb6150d8f36f92b1ba869a748ee06567d004758884ba3119fd5de016fb3c4d4e3b215'
