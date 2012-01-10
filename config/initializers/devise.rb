685bf993f375b2f8c33bd9ded523d51a7d3fa789
bzhang <bzhang@optaros.com>
2012-01-10 11:10:59 -0500

    fixing api keys

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,7 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-   config.omniauth :facebook, FACEBOOK_API_USER, FACEBOOK_API_KEY
+   config.omniauth :facebook, ENV['FACEBOOK_API_USER'], ENV['FACEBOOK_API_KEY']
    
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
